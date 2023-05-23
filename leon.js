aa32beea8e75ccf7f38eaf55a130fe77c4c85e00
RobanSeyidoglu <robanseyidoglu@gmail.com>
2023-05-23 23:44:47 +0300

    make landing section and edit header icons functions by JS

--- leon.js
+++ leon.js
@@ -1,11 +1,22 @@
-// Some Functions In The Header 
+// Some Functions In The Header
 let links = document.querySelector(".links");
 let ul = document.querySelector("ul");
 let body = document.body;
 links.onclick = function () {
   ul.style.display = "block";
 };
-ul.onmouseleave = function () {
-  ul.style.display = "none";
+links.onclick = function () {
+  if (ul.style.display === "block") {
+    ul.style.display = "none";
+  } else {
+    ul.style.display = "block";
+  }
 };
+document.addEventListener("click", function (event) {
+  let targetElement = event.target;
+  let isClickedInsideMenu = links.contains(targetElement);
+  if (!isClickedInsideMenu) {
+    ul.style.display = "none";
+  }
+});
 // End Header
